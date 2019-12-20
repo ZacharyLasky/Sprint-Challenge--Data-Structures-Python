75b365b14d3e330eb54f2b11116c1c831c189599
Zach Lasky <zacharylasky123@gmail.com>
2019-12-20 13:24:12 -0500

    reverseList tests pass

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,4 +1,5 @@
 from doubly_linked_list import DoublyLinkedList
+import _collections
 
 
 class RingBuffer:
@@ -6,15 +7,26 @@ class RingBuffer:
         self.capacity = capacity
         self.current = None
         self.storage = DoublyLinkedList()
+        self.cache = _collections.OrderedDict()
 
     def append(self, item):
-        pass
+        try:
+            # pop the item off
+            value = self.cache.pop(item)
+            # set value to the front
+            #  1,2,3,4 will become 4,1,2,3
+            self.cache[item] = value
+            return value
+        # KeyError happens when item is not in dictionary
+        except KeyError:
+            return None
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
+        list_buffer_contents.append(self.cache)
 
         return list_buffer_contents
 
